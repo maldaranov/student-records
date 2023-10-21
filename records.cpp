c4d44f583d56a21c94d677a20b79bf1b8de9bcca
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-20 22:35:43 -0400

    Adds the Course class implementation. Adds the Grade class implementation.

--- records.cpp
+++ records.cpp
@@ -14,3 +14,41 @@ string Student::get_name()
 {
 	return name;
 }
+
+Course::Course(int the_id, string the_name, unsigned char the_credits)
+{
+	id = the_id;
+	name = the_name;
+	credits = the_credits;
+}
+int Course::get_id()
+{
+	return id;
+}
+string Course::get_name()
+{
+	return name;
+}
+int Course::get_credits()
+{
+	return credits;
+}
+
+Grade::Grade(int the_student_id, int the_course_id, unsigned char the_grade)
+{
+	student_id = the_student_id;
+	course_id = the_course_id;
+	grade = the_grade;
+}
+int Grade::get_student_id()
+{
+	return student_id;
+}
+int Grade::get_course_id()
+{
+	return course_id;
+}
+unsigned char Grade::get_grade()
+{
+	return grade;
+}
