e1aecd6dfba0de1f088caf09d9d5c5f219be9df0
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-21 19:32:18 -0400

    Adds the initialize function to populate the instance of StudentRecords with test cases.

--- student-records.cpp
+++ student-records.cpp
@@ -2,7 +2,28 @@
 #include "records.h"
 using namespace std;
 
+void initialize_records();
+StudentRecords record_1;
+
 int main()
 {
 	return 0;
+}
+
+void initialize_records()
+{
+	record_1.add_student(1, "John Smith");
+	record_1.add_student(2, "Peter Parker");
+
+	record_1.add_course(1, "Algebra", 5);
+	record_1.add_course(2, "Physics", 4);
+	record_1.add_course(3, "English", 3);
+	record_1.add_course(4, "Economics", 4);
+
+	record_1.add_grade(1, 1, 'B');
+	record_1.add_grade(1, 2, 'A');
+	record_1.add_grade(1, 3, 'C');
+	record_1.add_grade(2, 1, 'A');
+	record_1.add_grade(2, 2, 'A');
+	record_1.add_grade(2, 4, 'B');
 }
