f05447fb05d9618965ce2637bdf9e91c0bdcd619
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-20 23:12:30 -0400

    Adds test-case-1 for testing the class methods.

--- student-records.cpp
+++ student-records.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "records.h"
+using namespace std;
+
+void test_1();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test_1();
 }
 
+void test_1()
+{
+	Student s1(1, "Maxim Aldaranov");
+	Course c1(15, "C++ 101", 3);
+	Grade g1(1, 15, 'A');
+	cout << "Student Name: " << s1.get_name() << endl;
+	cout << "Course: " << c1.get_name() << endl;
+	cout << "Credits: " << c1.get_credits() << endl;
+	cout << "Grade: " << g1.get_grade() << endl;
+}
