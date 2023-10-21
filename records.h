63b133da747c2dd9952673b1db549183d38343cc
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-20 22:10:56 -0400

    Adds the Student class declaration.

--- records.h
+++ records.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <string>
+
+class Student
+{
+private:
+	int id;
+	std::string name;
+
+public:
+	Student(int the_id, std::string the_name);
+	int get_id();
+	std::string get_name();
+};
